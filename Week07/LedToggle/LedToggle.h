0cfac2a7a88500d3b9582318b564cb7f6ba0025f
kennel <kennel0356@gmail.com>
2025-05-22 20:14:58 +0900

    move

---
+++ Week07/LedToggle/LedToggle.h
@@ -0,0 +1,18 @@
+#ifndef LedToggle_h
+#define LedToggle_h
+
+#include "Arduino.h"
+
+class LedToggle {
+	public:
+		LedToggle(int pin); //생성자 
+		LedToggle(int pin, unsigned long delayTime);
+		void toggle();		//LED 상태 토글 
+		
+		private:
+			int _pin;
+			bool _state;
+			unsigned long _delayTime;
+};
+
+#endif
